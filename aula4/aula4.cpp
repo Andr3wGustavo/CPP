98c41278055f102343ce5a1e67dba71d0d4a2b15
Andr3w Gustavo <andreadgustavo@gmail.com>
2021-09-18 23:20:05 -0300

    estudos 19/09

---
+++ aula4/aula4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	// tipo, nome
+	// tipo nome = valor;
+
+	int vidas = 0; //10, 25, 100
+	char letra = 'B'; //'b', 'c' 
+	char letras[20];
+	double decimal = 5.2; // 2.4999999
+	float decimal2 = 5.2; // 2.5
+	bool vivo = true; // true, false
+	string nome = "andre" ; // texto
+	
+	cout << "Digite o numero de vidas: ";
+	cin >> vidas;
+	cout << vidas;
+	
+	/*cout << nome << "\n";
+	cout << letra << "\n";
+	cout << vidas << "\n";
+	cout << vivo << "\n";
+	cout << decimal << "\n";*/
+	
+	
+	
+	return 0;
+}
