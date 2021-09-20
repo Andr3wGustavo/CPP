2307e9692431f26c00437428b2aca963c63516b4
Andr3w Gustavo <andreadgustavo@gmail.com>
2021-09-19 21:18:56 -0300

    estudos 19/09

---
+++ aula5/aula5.cpp
@@ -0,0 +1,20 @@
+// declaração multipla de variaveis
+
+#include <iostream>
+
+using namespace std;
+
+
+#define pi 3.141592  //define a constante
+#define canal cout << "Canal Nick mira" // define o comando como uma constante
+
+int main(){
+	
+	int vidas = 3, tiros = 500, life = 100; // declarção de 3 variaveis do mesmo tipo
+	
+	cout << pi << "\n";
+	
+	canal;
+	
+	return 0;
+}
