4c8aba18223864d0cc150cc83d398d9257a65948
Andr3w Gustavo <andreadgustavo@gmail.com>
2021-11-03 23:30:14 -0300

    estudos 03/11

---
+++ aula7/aula7.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	
+	int n1, n2;
+	n1 = 0;
+	n2 = 10;
+	
+	// incremento de valor em uma variavel
+	cout << n1 << "\n\n";
+	
+	//n1 = n1 + 5; n1 += 5;
+	//n1++; add 1
+	//n1*=10; n1/=10; 
+	
+
+	n1++; 
+	
+	cout << n1 << "\n\n";
+	
+		
+	return 0;
+	
+	
+}
