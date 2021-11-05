2a144ebee16d960ecb0fbb81a6908f9eabbd64c5
Andr3w Gustavo <andreadgustavo@gmail.com>
2021-11-05 13:52:35 -0300

    estudos 5/11

---
+++ aula8/aula8.cpp
@@ -0,0 +1,21 @@
+// inversão de valor de uma variavel
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int num = 10;
+	
+	cout << num << "\n\n";
+	
+	num *= -1; // ou podemos usar num= -num
+	
+	cout << num << "\n\n";
+	
+	return 0;
+	
+}
+
+
