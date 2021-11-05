0241fb8c0dc1288b8fc501a070595070c19d4809
Andr3w Gustavo <andreadgustavo@gmail.com>
2021-11-05 18:12:56 -0300

    estudos 05/11/2021

---
+++ aula9/aula9.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num = 10;
+    int num2 = 14;
+    if(num > num2){
+        cout << "True Result!";
+    }else{
+        cout << "False Result";
+    }
+
+    return 0;
+}
