b0c3f17521896f3df522ed82596e705e6e8d0012
Andr3w Gustavo <andreadgustavo@gmail.com>
2021-11-07 15:50:57 -0300

    estudos 07/11/2021

---
+++ aula13/aula13.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+
+    int transport;
+    char opc;
+
+    start:
+
+    system("cls");
+
+    cout << "[1]=Car, [2]=Bike, [3]=Airplane, [4]Helicopter\n";
+    cout << "Select a transport way: ";
+    cin >> transport;
+
+    switch(transport){
+        case 1:
+        case 2:
+            cout << "Terrestrial way\n";
+            switch(transport){
+                case 1:
+                    cout << "You selected: Car\n";
+                    break;
+                case 2:
+                    cout << "You selected: Bike\n";
+                    break;
+            }
+            break;
+        case 3:
+        case 4:
+            cout << "Air way\n";
+            switch(transport){
+            case 3:
+                cout << "You selected: Airplane\n";
+                break;
+            case 4:
+                cout << "You selected> Helicopter\n";
+                break;
+            }
+            break;
+    }
+
+    cout << "\nWanna continue? [y/n]: ";
+    cin >> opc;
+
+    if(opc=='Y' || opc=='y'){
+        goto start;
+    }
+
+	return 0;
+}
