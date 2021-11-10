701ed71f907769c6e9aed7bfa9bfc102c014ce57
Andr3w Gustavo <andreadgustavo@gmail.com>
2021-11-10 14:12:45 -0300

    estudos 10/11/2021

--- aula19/aula19.cpp
+++ aula19/aula19.cpp
@@ -4,7 +4,19 @@ using namespace std;
 
 int main(){
 
+    int matriz[4][4];
+    int l,c;
 
-
+   for(l=0;l<4;l++){
+        for(c=0;c<4;c++){
+            matriz[l][c]=l;
+        }
+   }
+    for(l=0;l<4;l++){
+        for(c=0;c<4;c++){
+            cout << matriz[l][c] << " ";
+        }
+        cout << "\n";
+    }
 	return 0;
 }
